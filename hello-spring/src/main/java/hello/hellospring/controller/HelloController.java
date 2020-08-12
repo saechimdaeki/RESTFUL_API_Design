c24029db7f529d8044248d3af59bc2f4f01bf76d
김준성 <anima94@naver.com>
2020-08-12 17:59:05 +0900

    영한님의 spring MVC!

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    /*컨트롤러에서 리턴 값으로 문자를 반환하면
+    뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다*/
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("data","spring!!!!!!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name,Model model)
+    {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+}
